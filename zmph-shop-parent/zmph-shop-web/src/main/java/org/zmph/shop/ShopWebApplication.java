b9951409b1f2b9df3bfb000a57454af2279ee350
1843080373 <42103600+1843080373@users.noreply.github.com>
2019-04-11 11:34:21 +0800

    Update ShopWebApplication.java

--- zmph-shop-parent/zmph-shop-web/src/main/java/org/zmph/shop/ShopWebApplication.java
+++ zmph-shop-parent/zmph-shop-web/src/main/java/org/zmph/shop/ShopWebApplication.java
@@ -2,13 +2,14 @@ package org.zmph.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class ShopWebApplication 
 {
